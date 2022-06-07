b97d1fc296ce93433b940b822c0b0da01e945301
Waseem Idelbi <idelbiw@gmail.com>
2022-06-06 22:08:18 -0400

    Created a new file with a struct to model the Bearer token for authentication

---
+++ Gigs/Gigs/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Waseem Idelbi on 6/6/22.
+//
+
+import Foundation
+
+struct Bearer {
+    var id:     Int
+    var userId: Int
+    var token:  String
+}
