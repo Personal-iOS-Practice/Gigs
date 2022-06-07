9f10f85e940305b897799e88633961f5806ecd99
Waseem Idelbi <idelbiw@gmail.com>
2022-06-06 21:43:57 -0400

    Added the readme and the api files from the repo, and created the project

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Waseem Idelbi on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
