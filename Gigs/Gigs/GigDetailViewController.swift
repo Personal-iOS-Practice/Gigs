2fb381670e5f5b3705f753e34cfecfc85033cb2a
Waseem Idelbi <idelbiw@gmail.com>
2022-06-14 22:29:57 -0400

    Added  a new view controller for creating new gigs and viewing existing ones with all UI elements implemented and a wired up with a new VC file

---
+++ Gigs/Gigs/GigDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Waseem Idelbi on 6/14/22.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+//MARK: - IBOutlets
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+//MARK: - IBActions
+    // When the user taps the save button
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
